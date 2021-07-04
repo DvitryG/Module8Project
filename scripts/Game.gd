35ff35f6b4aea9aa03566090cd331c1719130f2c
DvitryG <dmitry135531@gmail.com>
2021-07-04 09:25:34 +0300

    финал x2

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var pause = false;
+var x
+var z
 
 func _ready():
 	$Pause.visible = pause
@@ -8,16 +10,57 @@ func _ready():
 	$Pause/Panel/VBoxContainer/SettingsButton.connect("pressed", self, "open_settings")
 	$Pause/Panel/VBoxContainer/ExitButton.connect("pressed", self, "exit_game")
 	settings()
-	
+	x = 0
+	z = 0
 	pass
 
 func _process(delta):
 	if (Input.is_action_just_pressed("ui_cancel")):
 		continue_game()
 	GameData.mainData.xPos = $M113.get_global_transform().origin.x
-	GameData.mainData.yPos = $M113.get_global_transform().origin.y
+	GameData.mainData.yPos = $M113.get_global_transform().origin.z
+	change_parameters()
+	if (!GameData.mainData.infiniteMod):
+		check_final()
 	pass
 
+func check_final():
+	if (GameData.mainData.score >= 1200):
+		GameData.mainData.health = 100;
+		GameData.mainData.petrol = 100;
+		$GamePanel/FinLabel.visible = true
+		$GamePanel/FinLabel.text = tr("SCORE_FIN")
+		pass
+	if (GameData.mainData.health <= 0):
+		$GamePanel/FinLabel.visible = true
+		$GamePanel/FinLabel.text = tr("HEALTH_FIN")
+		get_tree().paused = true
+		pass
+	if (GameData.mainData.petrol <= 0):
+		$M113.enginePower = 0
+		$GamePanel/FinLabel.visible = true
+		$GamePanel/FinLabel.text = tr("PETROL_FIN")
+		#get_tree().paused = true
+		pass
+	pass
+
+func change_parameters():
+	if (!GameData.mainData.infiniteMod):
+		if (Input.is_action_pressed("forward_move") || Input.is_action_pressed("back_move") || Input.is_action_pressed("left_move") || Input.is_action_pressed("right_move")):
+			GameData.mainData.petrol -= 0.01
+		else:
+			GameData.mainData.health -= 0.01
+		$GamePanel/Health/Count.text =  str(round(GameData.mainData.health))
+		$GamePanel/Petrol/Count.text = str(round(GameData.mainData.petrol))
+		$GamePanel/Petrol/Count.text = str(round(GameData.mainData.petrol))
+	
+	GameData.mainData.score += sqrt((GameData.mainData.xPos - x)*(GameData.mainData.xPos - x)+(GameData.mainData.yPos - z)*(GameData.mainData.yPos - z))
+	x = GameData.mainData.xPos
+	z = GameData.mainData.yPos
+	
+	$GamePanel/Score/Count.text = str(round(GameData.mainData.score))
+	
+	pass
 
 func settings():
 	if (Settings.blur):
--- scripts/GamePanel.gd
+++ scripts/GamePanel.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var x
-var y
+var z
 
 func _ready():
 	print(GameData.mainData.score)
@@ -10,17 +10,17 @@ func _ready():
 	$Petrol/Count.text = str(GameData.mainData.petrol)
 	$FinLabel.visible = false
 	x = 0
-	y = 0
+	z = 0
 	pass
 
 func _process(delta):
-	change_parameters()
-	if (!GameData.mainData.infiniteMod):
-		check_final()
+	#change_parameters()
+	#if (!GameData.mainData.infiniteMod):
+	#	check_final()
 	pass
 
 func check_final():
-	if (GameData.mainData.score >= 5800):
+	if (GameData.mainData.score >= 1200):
 		GameData.mainData.health = 100;
 		GameData.mainData.petrol = 100;
 		$FinLabel.visible = true
@@ -41,15 +41,17 @@ func check_final():
 func change_parameters():
 	if (!GameData.mainData.infiniteMod):
 		if (Input.is_action_pressed("forward_move") || Input.is_action_pressed("back_move") || Input.is_action_pressed("left_move") || Input.is_action_pressed("right_move")):
-			GameData.mainData.petrol -= 0.005
+			GameData.mainData.petrol -= 0.01
 		else:
-			GameData.mainData.health -= 0.005
+			GameData.mainData.health -= 0.01
+		$Health/Count.text =  str(round(GameData.mainData.health))
+		$Petrol/Count.text = str(round(GameData.mainData.petrol))
+		$Petrol/Count.text = str(round(GameData.mainData.petrol))
 	
-	GameData.mainData.score += sqrt((GameData.mainData.xPos - x)*(GameData.mainData.xPos - x)+(GameData.mainData.yPos - y)*(GameData.mainData.yPos - y))
+	GameData.mainData.score += sqrt((GameData.mainData.xPos - x)*(GameData.mainData.xPos - x)+(GameData.mainData.yPos - z)*(GameData.mainData.yPos - z))
 	x = GameData.mainData.xPos
-	y = GameData.mainData.yPos
+	z = GameData.mainData.yPos
 	
-	$Score/Count.text = str(GameData.mainData.score)
-	$Health/Count.text =  str(GameData.mainData.health)
-	$Petrol/Count.text = str(GameData.mainData.petrol)
+	$Score/Count.text = str(round(GameData.mainData.score))
+	
 	pass
